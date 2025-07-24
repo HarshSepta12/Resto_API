87fae30ead23ea2f68a50992f959af303f85d06a
HarshSepta12 <harshservi48@gmail.com>
2025-07-24 12:09:21 +0530

    Implement Table Book

--- utils/SendEmail.js
+++ utils/SendEmail.js
@@ -6,16 +6,16 @@ export const sendEmail = async (to, subject, htmlContent) => {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.EMAIL_USER,     // Your Gmail (e.g. yourname@gmail.com)
-        pass: process.env.EMAIL_PASS,     // App Password from Google (16-character)
+        user: process.env.EMAIL_USER,    
+        pass: process.env.EMAIL_PASS,   
       },
     });
-console.log("EMAIL:", process.env.EMAIL_USER);
-console.log("PASS:", process.env.EMAIL_PASS);
+// console.log("EMAIL:", process.env.EMAIL_USER);
+// console.log("PASS:", process.env.EMAIL_PASS);
 
     // Mail options
     const mailOptions = {
-      from: `"My Restaurant" <${process.env.EMAIL_USER}>`,
+      from: `"Shree Aaiji Restaurant" <${process.env.EMAIL_USER}>`,
       to,
       subject,
       html: htmlContent,
